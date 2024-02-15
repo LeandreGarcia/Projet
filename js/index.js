b3c1f697ff7d56fa2e12f6e78036f0a94f870462
kadeidja <kadeidja.cisse@gmail.com>
2024-02-16 00:46:10 +0100

    changes added to the branch

--- js/index.js
+++ js/index.js
@@ -1,12 +1,54 @@
-fetch('http://jsonplaceholder.typicode.com/users')
-.then(response => response.json())
-.then(data =>{
-    data.forEach(user =>{
-        const paragrapheTxt = document.createElement('p')
-        paragrapheTxt.textContent = user.name
-        document.body.appendChild(paragrapheTxt)
-    }
-    )
-})
+const apiKey = "8ffb64bee3a847ce9b9102913241502";
 
-//https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}
+function searchWeather() {
+    const cityName = document.getElementById("searchInput").value;
+    const apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + cityName + "&lang=fr";
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            const weatherData = {
+                cityName: data.location.name,
+                country: data.location.country,
+                temperature: data.current.temp_c + "°c",
+                weatherImage: data.current.condition.icon,
+                humidity: data.current.humidity + "%",
+                windSpeed: Math.floor(data.current.wind_kph) + " Km/H",
+                meteo: data.current.condition.text
+            };
+            //console.log(weatherData.cityName)
+
+            displayWeather(weatherData);
+            //outputElement.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const weatherInfoContainer = document.getElementById("weatherInfo");
+            weatherInfoContainer.innerHTML = ` <p>Veuillez entrer une ville valide</p>`;
+        })
+}
+
+function displayWeather(data) {
+    const weatherInfoContainer = document.getElementById("weatherInfo");
+    weatherInfoContainer.innerHTML = `
+        <div class="weather-info">
+            <h2>Voici la météo pour ${data.cityName}, ${data.country}</h2>
+            <img src="https:${data.weatherImage}" alt="Weather">
+            <p>${data.meteo}</p>
+            <p>Température: ${data.temperature}</p>
+            <p>Taux d'humidité: ${data.humidity}</p>
+            <p>Vent: ${data.windSpeed}</p>
+        </div>
+    `;
+}
+
+document.getElementById("searchInput").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        searchWeather();
+    }
+});
