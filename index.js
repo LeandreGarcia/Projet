14e67344cb9f1366244f80c147003ecdc48b10c8
LeandreGarcia <leandre.gar@gmail.com>
2024-02-15 17:14:47 +0100

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
+const apiKey = "8ffb64bee3a847ce9b9102913241502";
+
 function searchWeather() {
     const cityName = document.getElementById("searchInput").value;
+    const apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + cityName + "&lang=fr";
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            const weatherData = {
+                cityName: data.location.name,
+                country: data.location.country,
+                temperature: data.current.temp_c + "°c",
+                weatherImage: data.current.condition.icon,
+                humidity: data.current.humidity + "%",
+                windSpeed: Math.floor(data.current.wind_kph) + " Km/H",
+                meteo: data.current.condition.text
+            };
+            //console.log(weatherData.cityName)
+
+            displayWeather(weatherData);
+            //outputElement.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const weatherInfoContainer = document.getElementById("weatherInfo");
+            weatherInfoContainer.innerHTML = ` <p>Veuillez entrer une ville valide</p>`;
+        })
     // Replace the below line with your API call to fetch weather data for the entered city
     // For simplicity, I'm assuming the weather data is retrieved from an API
-    const weatherData = {
-        cityName: "Paris",
-        temperature: "20°C",
-        weatherImage: "cloudy.png",
-        humidity: "65%",
-        windSpeed: "10 km/h"
-    };
 
-    displayWeather(weatherData);
 }
+//searchWeather();
 
 function displayWeather(data) {
     const weatherInfoContainer = document.getElementById("weatherInfo");
     weatherInfoContainer.innerHTML = `
         <div class="weather-info">
-            <h2>Voici la météo pour ${data.cityName}</h2>
+            <h2>Voici la météo pour ${data.cityName}, ${data.country}</h2>
             <img src="${data.weatherImage}" alt="Weather">
+            <p>${data.meteo}</p>
             <p>Température: ${data.temperature}</p>
             <p>Taux d'humidité: ${data.humidity}</p>
             <p>Vent: ${data.windSpeed}</p>
         </div>
     `;
 }
+
+document.getElementById("searchInput").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        searchWeather();
+    }
+});
