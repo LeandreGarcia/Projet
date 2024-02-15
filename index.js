07099fad0296f41c106596e14c5f727197360305
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-02-15 14:49:15 +0100

    ajout api

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+const apiKey = "8ffb64bee3a847ce9b9102913241502";
+
 function searchWeather() {
+    let city = "paris"
     const cityName = document.getElementById("searchInput").value;
+    const apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + city;
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            const weatherData = {
+                cityName: data.location.name,
+                temperature: data.current.temp_c + "°c",
+                weatherImage: data.current.condition.icon,
+                humidity: data.current.humidity +"%",
+                windSpeed: Math.floor(data.current.wind_kph)+" Km/H"
+            };
+            console.log(weatherData.cityName)
+
+            displayWeather(weatherData);
+            //outputElement.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
     // Replace the below line with your API call to fetch weather data for the entered city
     // For simplicity, I'm assuming the weather data is retrieved from an API
-    const weatherData = {
-        cityName: "Paris",
-        temperature: "20°C",
-        weatherImage: "cloudy.png",
-        humidity: "65%",
-        windSpeed: "10 km/h"
-    };
 
-    displayWeather(weatherData);
 }
+searchWeather();
 
 function displayWeather(data) {
     const weatherInfoContainer = document.getElementById("weatherInfo");
